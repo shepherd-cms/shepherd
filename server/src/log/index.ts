5ca2d2e5c5af09dda962496d6077a30891526ed2
Alex Regan <alex.joseph.regan@gmail.com>
2019-02-23 19:31:23 -0700

    fix: broken import path
    Signed-off-by: Alex Regan <alex.joseph.regan@gmail.com>

--- server/src/log/index.ts
+++ server/src/log/index.ts
@@ -1,8 +1,8 @@
-import { LogLevel, Logger } from './log.interface';
-import { ConsoleLogger } from './console';
+import { LogLevel, Logger } from "./log.interface";
+import { ConsoleLogger } from "./Console";
 
 let logger: Logger = new ConsoleLogger(LogLevel.All);
-if (process.env.NODE_ENV === 'production') {
+if (process.env.NODE_ENV === "production") {
   logger = new ConsoleLogger(LogLevel.Error);
 }
 
