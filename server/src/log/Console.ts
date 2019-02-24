b4067b917dcc80f1686dc65b591c9f321c16baee
Alex Regan <alex.joseph.regan@gmail.com>
2019-02-23 17:11:36 -0700

    chore: adds comments & fmt for console logger
    Signed-off-by: Alex Regan <alex.joseph.regan@gmail.com>

--- server/src/log/Console.ts
+++ server/src/log/Console.ts
@@ -1,7 +1,13 @@
-import { LogLevel, Logger, levelSatisfies } from './log.interface';
+import { LogLevel, Logger, levelSatisfies } from "./log.interface";
 
-const padZero = (n: number) => n.toString().padStart(2, '0');
+const padZero = (n: number) => n.toString().padStart(2, "0");
 
+/**
+ * ConsoleLogger is an implementation of the Logger interface that just
+ * delegates to various `console` methods. It uses methods as arrow function
+ * properties so that all callers need not concern themselves with gotchas
+ * around managing context for `this`.
+ */
 export class ConsoleLogger implements Logger {
   constructor(private level: number) {}
 
@@ -23,41 +29,41 @@ export class ConsoleLogger implements Logger {
     if (!levelSatisfies(this.level, LogLevel.Fatal)) {
       return;
     }
-    console.error(this.prefix('fatal'), ...values);
+    console.error(this.prefix("fatal"), ...values);
   };
 
   error = (...values: any[]) => {
     if (!levelSatisfies(this.level, LogLevel.Error)) {
       return;
     }
-    console.error(this.prefix('error'), ...values);
+    console.error(this.prefix("error"), ...values);
   };
 
   warn = (...values: any[]) => {
     if (!levelSatisfies(this.level, LogLevel.Warn)) {
       return;
     }
-    console.warn(this.prefix('warn'), ...values);
+    console.warn(this.prefix("warn"), ...values);
   };
 
   info = (...values: any[]) => {
     if (!levelSatisfies(this.level, LogLevel.Info)) {
       return;
     }
-    console.info(this.prefix('info'), ...values);
+    console.info(this.prefix("info"), ...values);
   };
 
   debug = (...values: any[]) => {
     if (!levelSatisfies(this.level, LogLevel.Debug)) {
       return;
     }
-    console.debug(this.prefix('debug'), ...values);
+    console.debug(this.prefix("debug"), ...values);
   };
 
   trace = (...values: any[]) => {
     if (!levelSatisfies(this.level, LogLevel.Trace)) {
       return;
     }
-    console.trace(this.prefix('trace'), ...values);
+    console.trace(this.prefix("trace"), ...values);
   };
 }
